b0aac1c7caa0ceb778df89df237eb79feaaeee19
stagib <147700418+stagib@users.noreply.github.com>
2025-04-07 20:48:23 +1000

    Refactor utils file

--- app/routers/t.py
+++ app/routers/t.py
@@ -1,14 +1,57 @@
 import requests
+import asyncio
+import aiohttp
+import torch
+import clip
 from fastapi import APIRouter, Query, Depends
 from sqlalchemy.orm import Session
+from PIL import Image
+from io import BytesIO
 
 from app.config import settings
 from app.database import get_db
 from app.models import Post
-from app.utils import fetch_all_images, get_image_vector
 
 
 router = APIRouter()
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+
+async def fetch_image(session, post):
+    try:
+        async with session.get(post.get("file_url"), timeout=30) as res:
+            if res.status == 200:
+                img_data = await res.read()
+                return {
+                    "img": Image.open(BytesIO(img_data)).convert("RGB"),
+                    "id": post.get("id"),
+                }
+    except Exception as e:
+        return None
+    return None
+
+
+async def fetch_all_images(post_data):
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_image(session, post) for post in post_data]
+        images = await asyncio.gather(*tasks)
+        map = {}
+        for image in images:
+            if image is None or not image.get("img"):
+                continue
+            map[image.get("id")] = image.get("img")
+        return map
+
+
+def get_image_vector(image):
+    try:
+        input = preprocess(image).unsqueeze(0).to(device)
+        with torch.no_grad():
+            vector = model.encode_image(input).cpu().numpy().tolist()[0]
+            return vector
+    except Exception as e:
+        return None
 
 
 @router.get("/test")
--- app/utils.py
+++ app/utils.py
@@ -1,59 +1,15 @@
-import asyncio
-import aiohttp
-import torch
-import clip
 import jwt
 from datetime import datetime, timezone, timedelta
 from argon2 import PasswordHasher
 from typing import Annotated
-from PIL import Image
 from fastapi import Depends, Cookie
 from sqlalchemy.orm import Session
-from io import BytesIO
 
 from app.database import get_db
 from app.models import User, Post
 from app.config import settings
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load("ViT-B/32", device=device)
 ph = PasswordHasher()
-
-
-async def fetch_image(session, post):
-    try:
-        async with session.get(post.get("file_url"), timeout=30) as res:
-            if res.status == 200:
-                img_data = await res.read()
-                return {
-                    "img": Image.open(BytesIO(img_data)).convert("RGB"),
-                    "id": post.get("id"),
-                }
-    except Exception as e:
-        return None
-    return None
-
-
-async def fetch_all_images(post_data):
-    async with aiohttp.ClientSession() as session:
-        tasks = [fetch_image(session, post) for post in post_data]
-        images = await asyncio.gather(*tasks)
-        map = {}
-        for image in images:
-            if image is None or not image.get("img"):
-                continue
-            map[image.get("id")] = image.get("img")
-        return map
-
-
-def get_image_vector(image):
-    try:
-        input = preprocess(image).unsqueeze(0).to(device)
-        with torch.no_grad():
-            vector = model.encode_image(input).cpu().numpy().tolist()[0]
-            return vector
-    except Exception as e:
-        return None
 
 
 def hash_password(password: str):
