fa71b883d30d7db32299bd04f580ec3f60f2d21e
stagib <147700418+stagib@users.noreply.github.com>
2025-04-12 10:57:15 +1000

    Black format

--- app/routers/user.py
+++ app/routers/user.py
@@ -21,10 +21,14 @@ router = APIRouter(tags=["User"])
 
 
 @router.post("/users")
-def register_user(response: Response, user: UserCreate, db: Session = Depends(get_db)):
+def register_user(
+    response: Response, user: UserCreate, db: Session = Depends(get_db)
+):
     db_user = db.query(User).filter(User.username == user.username).first()
     if db_user:
-        raise HTTPException(status_code=400, detail="Username is already taken")
+        raise HTTPException(
+            status_code=400, detail="Username is already taken"
+        )
 
     hashed_password = hash_password(user.password)
     new_user = User(username=user.username, password=hashed_password)
@@ -45,10 +49,14 @@ def register_user(response: Response, user: UserCreate, db: Session = Depends(ge
 def login(response: Response, user: UserCreate, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.username == user.username).first()
     if not db_user:
-        raise HTTPException(status_code=404, detail="Username or password is incorrect")
+        raise HTTPException(
+            status_code=404, detail="Username or password is incorrect"
+        )
 
     if not verify_password(db_user.password, user.password):
-        raise HTTPException(status_code=401, detail="Username or password is incorrect")
+        raise HTTPException(
+            status_code=401, detail="Username or password is incorrect"
+        )
 
     token = create_token(id=db_user.id)
     response.set_cookie(key="auth_token", value=token)
