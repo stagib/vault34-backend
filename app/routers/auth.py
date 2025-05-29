1668792e0603859752a185db0a77f0e79cd4ec50
stagib <147700418+stagib@users.noreply.github.com>
2025-05-29 14:45:16 +1000

    Fix verify-token validation error

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.schemas.user import UserBase
 from app.utils.auth import get_user
@@ -8,4 +8,7 @@ router = APIRouter(tags=["Auth"])
 
 @router.get("/verify-token", response_model=UserBase)
 def verify_auth_token(user: dict = Depends(get_user)):
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
     return user
